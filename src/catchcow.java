7741d9b5e9799c19c9f530dec692bef5dd6e48d4
ethayu <ethayu1@gmail.com>
2020-09-13 20:20:11 -0700

    commit

--- src/catchcow.java
+++ src/catchcow.java
@@ -1,2 +1,33 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class catchcow {
+
+    static int n, k;
+
+    public static void main (String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        int i = 0;
+        while (n != k) {
+            if (n < k) {
+                if (4*n < k) {
+                    n *= 2;
+                    i++;
+                } else if (Math.abs(k - 2*n) + 1> Math.abs(n - k)) {
+                    i += n - k;
+                    n = k;
+                } else {
+                    i += 1 + Math.abs(k - 2*n);
+                    n = k;
+                }
+            } else {
+                i += n - k;
+                n = k;
+            }
+        }
+        System.out.println(i);
+    }
+
 }
